98c4c42a74bb611087add39491002c54c536770f
fran-q <fdquiroga2017@gmail.com>
2023-02-09 00:02:52 -0300

    arreglando configuraciones

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 const path= require("path");
 
 app.use(express.static("public"));
 app.get("/", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/home.html"))} );
 app.get("/register", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/register.html"))});
 app.get("/login", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/login.html"))});
-app.listen(port,() => console.log("Pagina funcionando"));
+app.listen(port,() => console.log('Pagina funcionando en el puerto ${port}'));
 
 
 //app.use(express.static(path.resolve(__dirname,'../public')))
