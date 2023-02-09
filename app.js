084cba99e569a2cfea6c44889f57eff564b7ee9e
fran-q <fdquiroga2017@gmail.com>
2023-02-08 23:18:28 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path= require("path");
+
+app.use(express.static("public"));
+app.get("/", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/home.html"))} );
+app.get("/register", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/register.html"))});
+app.get("/login", (req,res) => { res.sendFile(path.resolve(__dirname,"./views/login.html"))});
+app.listen(port,() => console.log("Pagina funcionando"));
+
+
+//app.use(express.static(path.resolve(__dirname,'../public')))
+//npm i nodemon
+//npx nodemon app
