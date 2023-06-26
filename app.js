3e4685b472491ab9f4da0495f0a30b5559b9f608
fran-q <fdquiroga2017@gmail.com>
2023-06-26 02:00:37 -0300

    correccion package.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 const path= require("path");
 
 app.use(express.static("public"));
-app.listen(port,() => console.log('Pagina funcionando en http://localhost:3001/'));
+app.listen(port,() => console.log("Pagina funcionando en ${port}"));
 
 app.get("/", (req,res) => { 
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
