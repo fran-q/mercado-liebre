5b8fa8177c27a79904b709a20bb482a46f83440e
fran-q <fdquiroga2017@gmail.com>
2023-06-26 01:12:05 -0300

    Correccion de las vistas

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const path= require("path");
+
+app.use(express.static("public"));
+app.listen(port,() => console.log('Pagina funcionando en http://localhost:3001/'));
+
+app.get("/", (req,res) => { 
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+});
+app.get("/register", (req,res) => { 
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+});
+app.get("/login", (req,res) => { 
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+});
+
+
+//app.use(express.static(path.resolve(__dirname,'../public')))
+//npm i nodemon
+//npx nodemon app
